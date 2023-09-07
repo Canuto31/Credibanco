8a2cc58f3c43247f4c6c3127e62a0c63c6d80a24
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-09-07 10:16:38 -0500

    Definition of the queries for the data base

---
+++ src/main/resources/templates/drop.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS compra_producto;
+DROP TABLE IF EXISTS producto;
+DROP TABLE IF EXISTS compra;
+DROP TABLE IF EXISTS tarjeta;
+DROP TABLE IF EXISTS titular;
+DROP TABLE IF EXISTS estado_compra;
+DROP TABLE IF EXISTS estado_tarjeta;
---
+++ src/main/resources/templates/shema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE estado_tarjeta
+(
+    id     SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+
+);
+
+CREATE TABLE estado_compra
+(
+    id     SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE titular
+(
+    id       SERIAL PRIMARY KEY,
+    nombre   VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tarjeta
+(
+    id                INT PRIMARY KEY,
+    numero_tarjeta    VARCHAR(16)    NOT NULL,
+    titular           INT,
+    fecha_creacion    DATE           NOT NULL,
+    fecha_vencimiento DATE           NOT NULL,
+    saldo             DECIMAL(10, 2) NOT NULL,
+    estado_tarjeta    INT,
+    FOREIGN KEY (titular) REFERENCES titular (id),
+    FOREIGN KEY (estado_tarjeta) REFERENCES estado_tarjeta (id)
+);
+
+CREATE TABLE compra
+(
+    id                SERIAL PRIMARY KEY,
+    fecha_transaccion DATE NOT NULL,
+    tarjeta           INT,
+    estado_compra     INT,
+    FOREIGN KEY (tarjeta) REFERENCES tarjeta (id),
+    FOREIGN KEY (estado_compra) REFERENCES estado_compra (id)
+);
+
+CREATE TABLE producto
+(
+    id                  SERIAL PRIMARY KEY,
+    nombre              VARCHAR(255)   NOT NULL,
+    precio              DECIMAL(10, 2) NOT NULL,
+    cantidad_disponible INT            NOT NULL
+);
+
+CREATE TABLE compra_producto
+(
+    id_producto INT,
+    id_compra   INT,
+    cantidad    INT NOT NULL,
+    PRIMARY KEY (id_producto, id_compra),
+    FOREIGN KEY (id_producto) REFERENCES producto (id),
+    FOREIGN KEY (id_compra) REFERENCES compra (id)
+);
+
+--INSERT QUERY
